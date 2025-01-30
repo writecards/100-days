e41d4f108e4a8c047af8b3929bdba86c1ce9759c
writecards <rw2423@nyu.edu>
2025-01-30 22:12:37 +0800

    add oscillator

--- 040/pendulum.js
+++ 040/pendulum.js
@@ -1,16 +1,25 @@
 class Pendulum {
-  constructor(length) {
+  constructor(length, c, size, note) {
     this.length = length;
     this.angle = 45;
     this.angleV = 0;
     this.angleA = 0;
-    this.c = color(255);
+    this.c = c;
+    this.size = size;
+
+    this.note = note;
+    this.osc = new p5.Oscillator();
+    this.osc.start();
+    this.osc.freq(midiToFreq(this.note));
+    this.osc.amp(0);
   }
   update() {
     if (this.collison()) {
-      this.c = color(255, 0, 0);
+      this.nc = color(255);
+      this.osc.amp(1, 0.05);
     } else {
-      this.c = color(255);
+      this.nc = this.c;
+      this.osc.amp(0, 0.05);
     }
     this.angleA = (-gravity / this.length) * sin(this.angle);
     this.angleV += this.angleA;
@@ -20,11 +29,11 @@ class Pendulum {
     this.y = this.length * cos(this.angle);
   }
   display() {
-    stroke(this.c);
-    fill(this.c);
+    stroke(this.nc);
+    fill(this.nc);
 
     line(0, 0, this.x, this.y);
-    ellipse(this.x, this.y, 10, 10);
+    ellipse(this.x, this.y, this.size, this.size);
   }
 
   collison() {
--- 040/script.js
+++ 040/script.js
@@ -1,22 +1,38 @@
-let gravity = 1;
+let gravity = 0.8;
 
 let pendulums = [];
 let num = 15;
 let freq0 = 51; //freq of longest pendulum, 1/51. bc 51 swings per min
-let lengthMult = 20000000;
+let lengthMult = 40000000;
+let c = ["#0F5FA6", "#034001", "#B9BF04", "#F27405", "#F22816"];
+let osc;
+let midiNote = 60;
+//let midiNotes = [55, 52, 48, 45, 40, 43, 40, 36];
+let midiNotes = [44];
 
+// let midiNotes = [91, 88, 84, 79, 76, 72, 67, 64, 60, 55, 52, 48, 43, 40, 36];
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
+
   for (let i = 0; i < num; i++) {
     let period = 1 / (freq0 + i);
     let length = gravity * pow(period / TWO_PI, 2) * lengthMult;
-    pendulums[i] = new Pendulum(length);
-    console.log(length);
+    let s = 15 + i * 0.5;
+    pendulums[i] = new Pendulum(
+      length,
+      c[i % c.length],
+      s,
+      midiNotes[i % midiNotes.length]
+    );
   }
 }
 function draw() {
   background(0);
-
+  //   if (mouseIsPressed) {
+  //     osc.amp(1, 0.05);
+  //   } else {
+  //     osc.amp(0, 0.05);
+  //   }
   translate(width / 2, 0);
   for (let i = 0; i < pendulums.length; i++) {
     pendulums[i].update();
